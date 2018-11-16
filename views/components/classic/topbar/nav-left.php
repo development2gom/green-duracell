7899d20f43ca26a1e6c59dd37daf643f02a391bc
Humberto <humberto@2com.com.mx>
2018-11-16 09:59:46 -0600

    boton de logout

--- views/components/classic/topbar/nav-left.php
+++ views/components/classic/topbar/nav-left.php
@@ -5,4 +5,12 @@ use yii\helpers\Url;
   <h1 class="logo-duracell">
     <img src="<?= Url::base() ?>/webAssets/images/duracell.png" alt="">
   </h1>
+  <?php
+  if(!Yii::$app->user->isGuest){
+?>
+<a style="position: absolute;right: 23px;" class="btn btn-success" href="<?=Url::base()."site/logout"?>">Salir</a>
+<?php
+  }
+
+  ?>
 </div>
--- views/components/classic/topbar/nav-right.php
+++ views/components/classic/topbar/nav-right.php
@@ -5,4 +5,13 @@ use yii\helpers\Url;
   <h1 class="logo-duracell">
     <img src="<?= Url::base() ?>/webAssets/images/duracell.png" alt="">
   </h1>
+  <?php
+  if(!Yii::$app->user->isGuest){
+?>
+<a style="position: absolute;left: 23px;" class="btn btn-success" href="<?=Url::base()."site/logout"?>">Salir</a>
+<?php
+  }
+
+  ?>
+  
 </div>
